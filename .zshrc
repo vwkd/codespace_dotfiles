f06b965f9b766ffcdd07b9e4db0551ae568d52d6
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-09 12:01:19 +0400

    use install.sh

--- .zshrc
+++
@@ -1,2 +0,0 @@
-export DENO_INSTALL="/home/codespace/.deno"
-export PATH="$DENO_INSTALL/bin:$PATH"
---
+++ install.sh
@@ -0,0 +1,4 @@
+curl -fsSL https://deno.land/x/install/install.sh | sh
+
+export DENO_INSTALL="/home/codespace/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
